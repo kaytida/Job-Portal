e49bdedda8e2a77934836c48e555e4d8e226774a
Naga Sushanth Kumar Vuppala <NagaSushanth.Vuppala@straive.com>
2024-09-09 15:32:38 +0530

    hi

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import mysql.connector
-from googletrans import Translator
 import job_seeker
 import job_giver
 import admin
+
 
 # Function to connect to the MySQL database
 def connect_to_db():
@@ -27,49 +27,22 @@ def authenticate(email, password):
 
     return result
 
-# Helper function to translate text
-def translate_text(text, target_language):
-    translator = Translator()
-    translation = translator.translate(text, dest=target_language)
-    return translation.text
-
 # Main app function
 def main():
-    # Initialize the translator
-    translator = Translator()
 
-    # Language selection
-    language_options = {
-        "English": "en",
-        "Telugu": "te",
-        "Hindi": "hi"  # Added Hindi language option
-    }
-    selected_language = st.sidebar.selectbox("Choose Language", list(language_options.keys()))
-    selected_language_code = language_options[selected_language]
-    
-    # Initialize session state for role and language
+    # Initialize session state for role
     if 'role' not in st.session_state:
         st.session_state['role'] = None
-    if 'language' not in st.session_state:
-        st.session_state['language'] = selected_language_code
-
-    # Update session language if changed
-    if selected_language_code != st.session_state['language']:
-        st.session_state['language'] = selected_language_code
 
     # App title
-    st.title(translate_text("Job Portal", st.session_state['language']))
-
+    st.title("Job Portal")
+    
     if st.session_state['role'] is None:
         # User input for login
-        email_label = translate_text("Enter your Email ID", st.session_state['language'])
-        password_label = translate_text("Enter your Password", st.session_state['language'])
-        login_button = translate_text("Login", st.session_state['language'])
-        
-        email = st.text_input(email_label)
-        password = st.text_input(password_label, type="password")
+        email = st.text_input("Enter your Email ID")
+        password = st.text_input("Enter your Password", type="password")
 
-        if st.button(login_button):
+        if st.button("Login"):
             result = authenticate(email, password)
             user_role = ''
             if result:
@@ -88,7 +61,7 @@ def main():
                     # Rerun is unnecessary because the UI will update automatically
 
             else:
-                st.error(translate_text("Invalid email or password. Please try again.", st.session_state['language']))
+                st.error("Invalid email or password. Please try again.")
     else:
         # Display the correct page based on role
         if st.session_state['role'] == "job_seeker":
